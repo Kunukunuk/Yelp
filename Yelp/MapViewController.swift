fcc3686d14fbc77ea36ef6ac8149cc50ef68cd27
Kun Huang <kkun.huang@gmail.com>
2018-09-23 22:37:02 -0400

    testing map view annotation

--- Yelp/MapViewController.swift
+++ Yelp/MapViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
+
+    @IBOutlet weak var goToMapView: MKMapView!
     var locationManager : CLLocationManager!
     
     override func viewDidLoad() {
@@ -20,6 +21,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
         // Do any additional setup after loading the view.
         let centerLocation = CLLocation(latitude: 37.7833, longitude: -122.4167)
+        addAnnotationAtCoordinate(coordinate: CLLocationCoordinate2DMake(centerLocation.coordinate.latitude, centerLocation.coordinate.longitude))
         goToLocation(location: centerLocation)
         
         locationManager = CLLocationManager()
@@ -32,7 +34,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     func goToLocation(location: CLLocation) {
         let span = MKCoordinateSpanMake(0.1, 0.1)
         let region = MKCoordinateRegionMake(location.coordinate, span)
-        mapView.setRegion(region, animated: false)
+        goToMapView.setRegion(region, animated: false)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -45,8 +47,47 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         if let location = locations.first {
             let span = MKCoordinateSpanMake(0.1, 0.1)
             let region = MKCoordinateRegionMake(location.coordinate, span)
-            mapView.setRegion(region, animated: false)
+            goToMapView.setRegion(region, animated: false)
         }
+    }
+    
+    func addAnnotationAtCoordinate(coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "An annotation!"
+        goToMapView.addAnnotation(annotation)
+    }
+    
+    // add an annotation with an address: String
+    func addAnnotationAtAddress(address: String, title: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let placemarks = placemarks {
+                if placemarks.count != 0 {
+                    let coordinate = placemarks.first!.location!
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coordinate.coordinate
+                    annotation.title = title
+                    self.goToMapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "customAnnotationView"
+        
+        // custom image annotation
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        if (annotationView == nil) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        annotationView!.image = UIImage(named: "stars_5")
+        
+        return annotationView
     }
     
     /*
